a475757c348ada67c7f109a2fc89dc84988c1c47
Poga Po <poga.bahamut@gmail.com>
2017-01-01 01:40:33 +0800

    destroy update stream when connection lost

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ function Server (feed, opts) {
 
   this._server.on('connection', function (ws) {
     var updates = feed.list({live: true})
+    ws.on('close', function () {
+      updates.destory()
+    })
+
     updates.on('data', entry => {
       if (!opts.filter || (opts.filter && opts.filter(entry))) {
         feed.load(entry).then(item => {
           entry.item = item
           ws.send(JSON.stringify(entry))
-        }).catch(function (err) {
-          console.log('ws error', err)
         })
       }
     })
